693ff1fd7620e48b8995f0f17635b0c3d9db3c42
Rilso <53212931+chimmykk@users.noreply.github.com>
2023-08-05 21:48:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function ReactAppWithIframe() {
     <div>
       <h1>So Webpage is the main parent</h1>
       <div>
-      <iframe title="Webpage X" src="http://localhost:3001/" width="800" height="600"></iframe>
+      <iframe title="Webpage X" src="https://test1l.vercel.app" width="800" height="600"></iframe>
         <button onClick={() => window.location.href = 'https://www.google.com'}>Go to Google</button>
         <button onClick={() => window.location.href = 'https://www.facebook.com'}>Go to Facebook</button>
         <button onClick={() => window.location.href = 'https://www.youtube.com'}>Go to YouTube</button>
